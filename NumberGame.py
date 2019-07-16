ab8e09892721bb458cbf3f1c5b633b2723599952
cyunis <catherineyunis@gmail.com>
2019-07-15 18:08:00 -0700

    Update NumberGame.py

--- NumberGame.py
+++ NumberGame.py
@@ -1,50 +1,36 @@
 #code for the bisection number game, start with logical steps
 #step 1: initialize variables, import modules
 import random
-import bisect
 
-#step 2: generate strings for first interactions,
-#pull a random phrase from correct set
-intro_set = ['Nice to meet you, I\'m QT! Let\'s play a game! Think of a number between 1 and 100',
-             'Guess a number between 1 and 100!'] 
-    #make this a dictionary/set, giving each entry a line break makes it easier to read
-    #i can nest dictionaries
-random_phrase_ix = random.randrange(0,len(intro_set))
-intro_string = intro_set[random_phrase_ix]
-    #use get method for the intro set dict.get(key, set return value)
-start = input('Let\'s start! ') #type ok
-print(intro_string)
-
-#step 3: ask if guess is correct, use bisection alg
-    #identify variables to compare in boolean operations ex: guess, high bound, low bound, random_w_guess
-    #print statements after every line to follow logic flow when debugging
-print('Is this your guess?')
+nocounter = 0
+yescounter = 0
 high = 101
 low = -1
-half_guess = int((high-low)/2)
-random_guess = 
-print(half_guess)
 
-guess = bisect.bisect(guess_list, half_guess)+half_guess
-print(guess+low)
-
-#step 4: read response from participant
-#for now use button press
-
-#step 5: repeat - make the game ongoing
-    #use 0/1 or true/false instead of string
+#use 0/1 or true/false instead of string
+start = 'ok'
 while start == 'ok':
-    val = input('Is my guess correct? ')
-    if val == 'no':
-        val2 = input('higher or lower? ')
-        if val2 == 'higher':
-            guess_list = [guess,100]
-        if val2 == 'lower':
-            guess_list = [0,guess]
-        half_guess = int((high-low)/2)
-        guess = bisect.bisect(guess_list, half_guess)+half_guess
-        print(guess+guess_list[0])
-    if val == 'yes':
+    half_range = int((high-low)/2)
+    current = half_range+low
+    random_add = random.randrange(-half_range,half_range) #never add on outside of the guessing range
+    QT = current+random_add
+    print("QT Guess is: "+str(QT))
+    print("Current is {} and random addition is {}.".format(current,random_add))
+    print("High is {} and low is {}.".format(high,low))
+
+    val = input('Is my guess correct? (0 no, 1 yes) ')
+    if val is "0":
+        val2 = input('higher(1) or lower(0)? ')
+        nocounter += 1
+        if val2 is "1":
+            low = QT
+            yescounter += 1
+        if val2 is "0":
+            high = QT
+            nocounter += 1
+    else:
+        yescounter += 1
+        print("Number of yes: "+str(yescounter)+". Number of no: "+str(nocounter))
         print('I win!')
         quit()
         
