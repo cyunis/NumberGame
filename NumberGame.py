ab459325732941215fb3097c67ed8a1fc420501d
cyunis <catherineyunis@gmail.com>
2019-07-25 12:59:44 -0700

    Update NumberGame.py

--- NumberGame.py
+++ NumberGame.py
@@ -6,7 +6,26 @@
 import rospy
 from std_msgs.msg import String
 
-# class DataReporter(object):
+def callback(data):
+    strdata = str(data)
+
+    # hacky split
+    val = strdata.split(':')
+    val = val[1].split('\\t');
+    temp = val[0].split('"');
+
+    frame = int(temp[1])
+    state = int(val[1])
+    buttons = int(val[2])
+    print(frame, state, buttons)
+
+class globalCallback:
+    callback()
+    gbuttons = callback().buttons
+    def checking(self):
+        if 1 in gbuttons:
+            return 2
+    
 #     def __init__(self, filename):
 #         self._filename = filename
 #         # hacky split
@@ -21,19 +40,6 @@ from std_msgs.msg import String
 #     def report(self, message):
 #         open(self._filename).write(message)
     
-def callback(data):
-    strdata = str(data)
-
-    # hacky split
-    val = strdata.split(':')
-    val = val[1].split('\\t');
-    temp = val[0].split('"');
-
-    frame = int(temp[1])
-    state = int(val[1])
-    buttons = int(val[2])
-    print(frame, state, buttons)
-
 def listener():
     rospy.init_node('listener', anonymous=True)
     rospy.Subscriber('openwearable', String, callback)
@@ -77,7 +83,8 @@ print("""In this game I get to ask you questions, and you get to answer yes or n
 # listener()
 if __name__ == '__main__':
     listener()
-    if buttons is 1:
+    guess1 = GlobalCallback()
+    if guess1 is 2:
         print("Awesome! Now can you show me a thumbs down to say no?")
 # correctup = input('Was it a good thumbs up? ') #replace with line(s) reading button input = 1
 # if correctup is 'yes':
