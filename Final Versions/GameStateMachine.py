cd53910c9191875e1e7e5dc4f764cca113343173
Nathan Dennler <dennler@usc.edu>
2019-09-13 17:44:54 -0700

    added feedback function and started a stateMachine for the game.

---
+++ Final Versions/GameStateMachine.py
@@ -0,0 +1,4 @@
+import transitions
+
+class NumberGame:
+    
--- Final Versions/feedback.py
+++ Final Versions/feedback.py
@@ -1,58 +1,140 @@
-#feedback function
-def feedback_function(thumb_angle, gesture_time, time, name):
-    global speechSay_pub, encourage_dict, reward_dict, clarify_dict, feedback_dict, count, wrongcounter, supinate, pronate
-    #give each item weights and combine weights to make a %
-    #want reward to be 80-50% and encourage >80% always
-    #camera angle, GAS (fatigue), history of gestures, # of prompts
-    #camera angles should be matched to buckets on the GAS - need to see lit if standard #s for this (10% is 1, 20% is 2)
-        #these should be the most important factors to weight
-    #increase the encouragement when GAS, camera angle is worse and increase more if history shows a pattern of worsening    
-    #if high number of rewards maybe dont need to increase encouragement as much
-    #if a lot of clarification is needed, and bad history of gestures, more encouragement and more reward for lower GAS
-    #if history of gestures is bad but shows one good case give a reward
-    #history categories: 1)90% good and then 10% bad(sudden dip) 2)equal mixture of good or bad 3)no improvement 4)getting worse 5)getting better 6)90% bad then good
-    #1, 3, 4 - more encouragement. 2 - varied encouragement (maybe getting bored?). 5, 6 - more reward + encouragement.
-    #1, 6 - high weights.
-    #prompt categories: 1)a lot of clarification 2)a little clarification 3)less encouragement than normal 4)a lot of reward 5)a little reward
-    #1 - more encouragement (maybe tired?). 2 - more reward. 3 - varied encouragement.
-    
-    #make the buckets based on the GAS variables
-    bucket = determine_GAS_bucket(thumb_angle, gesture_time)
-    
-    historical_case = get_historical_case(bucket)
-    
-    # switch between historical cases to determine the increase / decrease
-    
-    timeScale = log(time) # get some reasonable quantification of the time
-    
-    #calculate the probability of different feedback
-    feedback_probability = timescale * CONSTANT + bucket * CONSTANT + CONSTANT
-    
-    #grab the specific behavior with the given probability
-    
-    #play the behavior
-    
-    
-    
-    
-    
-    
-    
-def determine_GAS_bucket(thumb_angle, gesture_time):
-    #place the GAS variables in an array and then find the index at which some combo of the thumb and time lies
-    #history depends on the actual angle not the GAS score
-    #do we want a seperate set of buckets for each time and angle ?? YES
-        #if so, how should we handle the "history case" later for things like they have been getting better at the angle, but the time is worse?
-    
-    GAS_scores = [10, 20, 30, 40, 50] # are these upper bounds on the ranges?? (also get these from the global values)
-    
-    for index in range(len(GAS_scores)):
-        if thumb_angle < GAS_scores[index]:
-            return index
-            
-    #if we get here, the thumb angle is higher than the highest GAS score        
-    return len(GAS_scores)
-    
-def get_historical_case(bucket)
-    #do we want this to be based on the bucket? or is it based on the raw thumb angles? or should it be time? some combo of the two?
-    pass
+import time
+import random
+import math
+
+class feedbackController:
+    def __init__(self):
+        self.supinateList = []
+        self.pronateList = []
+        self.performedBehaviors = [[],[],[],[],[]]
+
+        self.bucket1 = {
+            1: "clarify1",
+            2: "clarify2"
+        }
+
+
+        self.bucket2 = {
+            1: "encourage1",
+            2: "encourage2",
+            3: "encourage3",
+            4: "encourage4"
+        }
+
+        self.bucket3 = {
+            1: "encourageLess1",
+            2: "encourageLess2",
+            3: "encourageLess3"
+        }
+
+        self.bucket4 = {
+            1: "rewardLess1",
+            2: "rewardLess2",
+            3: "rewardLess3",
+            4: "rewardLess4",
+            5: "rewardLess5"
+        }
+
+        self.bucket5 = {
+            1: "reward1",
+            2: "reward2"
+        }
+
+    def chooseRandom(self, bucketNumber):
+        upperBound = 0
+        behaviorDict = {}
+
+        if(bucketNumber == 1):
+            upperBound = len(self.bucket1.keys())
+            behaviorDict = self.bucket1
+        if(bucketNumber == 2):
+            upperBound = len(self.bucket2.keys())
+            behaviorDict = self.bucket2
+        if(bucketNumber == 3):
+            upperBound = len(self.bucket3.keys())
+            behaviorDict = self.bucket3
+        if(bucketNumber == 4):
+            upperBound = len(self.bucket4.keys())
+            behaviorDict = self.bucket4
+        if(bucketNumber == 5):
+            upperBound = len(self.bucket5.keys())
+            behaviorDict = self.bucket5
+
+        if(upperBound == len(self.performedBehaviors[bucketNumber - 1])):
+            self.performedBehaviors[bucketNumber - 1] = []
+
+        behaviorNumber = random.randint(1,upperBound)
+        while(behaviorNumber in self.performedBehaviors[bucketNumber-1]):
+            behaviorNumber = random.randint(1,upperBound)
+
+        self.performedBehaviors[bucketNumber-1].append(behaviorNumber)
+        return behaviorDict[behaviorNumber]
+
+
+    def getBehavior(self, thumb_angle, gesture_time):
+        supinate = True
+        pronate = False
+
+        if(supinate):
+            appendValue(self.supinateList, thumb_angle)
+        elif(pronate):
+            appendValue(self.pronateList, thumb_angle)
+        else:
+            print('Uh-Oh, that is neither supinate nor pronate')
+
+        #current angle is the best seen so far
+        if(pronate and max(self.pronateList) == thumb_angle) or (supinate and min(self.supinateList) == thumb_angle):
+            behavior = self.chooseRandom(5) #maybe we want a seperate set of improved behaviors?
+
+        #make the buckets based on the GAS variables
+        angle_bucket = determine_angle_GAS_bucket(thumb_angle)
+        time_bucket = determine_time_GAS_bucket(gesture_time)
+        print('angle bucket is {}, time bucket is {}'.format(angle_bucket, time_bucket))
+        case = int(math.ceil((angle_bucket + time_bucket) / 2))
+
+        #return the behavior key
+        return self.chooseRandom(case)
+
+
+def appendValue(listToAppend, valueToAppend):
+    maxLength = 10 #parameter that can be tuned
+    listToAppend.append(valueToAppend)
+    if len(listToAppend) > maxLength:
+        listToAppend.pop(0)
+
+
+def determine_angle_GAS_bucket(thumb_angle):
+    #TODO: get these from a file!!!
+    GAS_angle_scores = [10, 20, 30, 40, 50] # are these upper bounds on the ranges?? (also get these from the global values)
+
+    for index in range(len(GAS_angle_scores)):
+        if thumb_angle < GAS_angle_scores[index]:
+            return index + 1
+    #if we get here, the thumb angle is higher than the highest GAS score
+    return len(GAS_angle_scores)
+
+def determine_time_GAS_bucket(gesture_time):
+    #TODO: get these from a file!!!
+    GAS_time_scores = [5, 4, 3, 2, 1] # are these upper bounds on the ranges?? (also get these from the global values)
+
+    for index in range(len(GAS_time_scores)):
+        if gesture_time > GAS_time_scores[index]:
+            return index + 1
+    #if we get here, the thumb angle is higher than the highest GAS score
+    return len(GAS_time_scores)
+
+
+
+
+# fb = feedbackController()
+# start_time = time.time()
+#
+# while 1:
+#     thumb_angle = random.randint(1,60)
+#     gesture_time = random.randint(1,50)*0.1
+#
+#     print('angle: {}, time: {}'.format(thumb_angle, gesture_time))
+#
+#     fb.getBehavior(thumb_angle, gesture_time, time.time() - start_time, 'nathan')
+#     for i in range(1,6):
+#         print('Bucket{}: {}'.format(i, fb.performedBehaviors[i-1]))
