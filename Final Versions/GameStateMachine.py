a33e1e2d5af25e3b86e77a780018450c1fc39063
Nathan Dennler <dennler@usc.edu>
2019-09-17 13:26:17 -0700

    added state diagram and statemachine

--- Final Versions/GameStateMachine.py
+++ Final Versions/GameStateMachine.py
@@ -1,4 +1,203 @@
 import transitions
+import time
+from transitions.extensions import GraphMachine as Machine
+from feedback import statementRandomizer
+import random
+import pygraphviz
+import sys
 
-class NumberGame:
-    
+import roslib; roslib.load_manifest('cordial_example')
+import rospy
+from cordial_core import RobotManager
+
+class NumberGameInteraction:
+    def __init__(self, use_qt_robot):
+        # rospy.init_node('qt_number_game')
+        # #set up publishers and subscribers
+        # #publishers
+        # self.audio_play_pub = rospy.Publisher('/qt_robot/audio/play', String, queue_size=10)
+        #
+        # self.right_pub = rospy.Publisher('/qt_robot/right_arm_position/command', Float64MultiArray, queue_size=1)
+        # self.left_pub = rospy.Publisher('/qt_robot/left_arm_position/command', Float64MultiArray, queue_size=1)
+        # self.head_pub = rospy.Publisher('/qt_robot/head_position/command', Float64MultiArray, queue_size=1)
+        #
+        # #subscribers
+        # self.thumb_sub = rospy.Subscriber("/thumb_result", String, self.process_thumb_data)
+        # self.beaglebone_sub = rospy.Subscriber("/openwearable_new", String, self.process_beaglebone_data)
+
+
+
+        #data to measure
+        self.start_time = time.time()
+        self.number_of_completed_games = 0
+        self.number_of_supinations = 0
+        self.number_of_pronations = 0
+
+        #gesture being made,
+
+        self.thumb_value = 0
+        self.gesture_time = 0
+
+        #data to make game run smoothly
+        self.statementRandomizer = statementRandomizer()
+        # self.robotManager = RobotManager('QT')
+
+        self.use_robot = use_qt_robot
+        self.guess_lower_bound = 1
+        self.guess_upper_bound = 50
+        self.guesses = []
+        self.number = 0
+        self.ready_to_move_on = True
+
+
+
+    #callback functions
+    def process_thumb_data(self, msg):
+        #TODO: implement
+        pass
+
+    def process_beaglebone_data(self, msg):
+        #TODO: implement
+        pass
+
+    #helper functions to make sure that transitions are executed correctly
+    def is_ready_to_move_on(self):
+        #return whether or not the state machine can continue
+        return self.ready_to_move_on
+
+    def reset_ready_to_move_on(self):
+        #called after every transition is completed
+        self.ready_to_move_on = False
+
+    def guess_equals_number(self):
+        return self.guesses[-1] == self.number
+
+    def give_feedback(self):
+        if(self.use_robot):
+            print(self.statementRandomizer.getResponseBehavior(self.thumb_angle, self.gesture_time))
+
+    def get_guess_statement(self):
+        print(self.statementRandomizer.chooseRandomStatement(0))
+
+    def give_feedback_calculate_guess(self):
+        self.give_feedback()
+        self.calculate_guess()
+
+
+    #logic for each state transition
+    def get_name(self):
+        self.name = raw_input('what is their name?')
+        if(self.name == 'abort'):
+            self.abort()
+
+    def give_instruction(self):
+        print('you will play a simple thumbs up/down game with me!')
+
+    def on_enter_practice_thumb_up(self):
+        print('practice thumb up')
+        res = input('did thumb go up?')
+        self.ready_to_move_on = res > 0
+
+    def practice_doesnt_go_up(self):
+        print('try to put your thumb up again')
+
+    def on_enter_practice_thumb_down(self):
+        print('practicing thumb down')
+        res = input('did thumb go down?')
+        self.ready_to_move_on = res > 0
+
+    def practice_doesnt_go_down(self):
+        print('try to put your thumb down again')
+
+    def on_enter_input_number(self):
+        self.number = input('what is the number being guessed?')
+
+    def calculate_guess(self):
+        half_range = int((self.guess_upper_bound - self.guess_lower_bound)/2)
+        ideal_guess = half_range+ self.guess_lower_bound
+        guess = ideal_guess + random.randint(-half_range, half_range)
+        while(guess in self.guesses):
+            guess = ideal_guess + random.randint(-half_range, half_range)
+        self.guesses.append(guess)
+
+    def on_enter_make_guess(self):
+        res = input('{}: is {} your number?'.format(self.statementRandomizer.chooseRandomStatement(0),self.guesses[-1]))
+        self.ready_to_move_on = res > 0
+
+    def incorrect_guess_response(self):
+        print('actually, let me ask in a different way...')
+
+    def on_enter_higher_or_lower(self):
+        print('is your number higher or lower than {}? (number is {})'.format(self.guesses[-1], self.number))
+        res = input('higher?')
+
+        if(self.guesses[-1] < self.number and res > 0):
+            self.ready_to_move_on = True
+            self.guess_lower_bound = self.guesses[-1]
+        elif(self.guesses[-1] > self.number and res < 1):
+            self.ready_to_move_on = True
+            self.guess_upper_bound = self.guesses[-1]
+
+    def incorrect_higher_lower(self):
+        print('hmmmm are you sure?')
+
+    def give_feedback(self):
+        if(self.use_robot):
+            print(self.statementRandomizer.getResponseBehavior(random.randint(1,60), random.randint(1,60)*0.1))
+
+
+    def on_enter_play_again(self):
+        print(self.statementRandomizer.performedBehaviors)
+
+        res = input('woohoo I won, want to play again?')
+
+        self.number_of_completed_games += 1
+        self.guesses = []
+        self.guess_lower_bound = 1
+        self.guess_upper_bound = 50
+        self.ready_to_move_on = res < 1
+
+    def on_enter_end(self):
+        print('thanks for playing!')
+        print('stats: games played: {}\ntime played: {}'.format(self.number_of_completed_games, time.time()-self.start_time))
+        sys.exit()
+
+
+
+states = ['get_name', 'practice_thumb_up', 'practice_thumb_down', 'input_number', 'make_guess', 'higher_or_lower', 'play_again', 'end']
+transitions = [
+                { 'trigger': 'advance', 'source': 'get_name', 'dest': 'practice_thumb_up', 'prepare':'get_name', 'before':'give_instruction'},
+
+                { 'trigger': 'advance', 'source': 'practice_thumb_up', 'dest': 'practice_thumb_down', 'conditions':'is_ready_to_move_on' },
+                { 'trigger': 'advance', 'source': 'practice_thumb_up', 'dest': 'practice_thumb_up', 'unless':'is_ready_to_move_on', 'before':'practice_doesnt_go_up'},
+
+                { 'trigger': 'advance', 'source': 'practice_thumb_down', 'dest': 'input_number', 'conditions':'is_ready_to_move_on' },
+                { 'trigger': 'advance', 'source': 'practice_thumb_down', 'dest': 'practice_thumb_down', 'unless':'is_ready_to_move_on', 'before':'practice_doesnt_go_down'},
+
+                { 'trigger': 'advance', 'source': 'input_number', 'dest': 'make_guess', 'before':'calculate_guess' },
+
+                { 'trigger': 'advance', 'source': 'make_guess', 'dest': 'play_again', 'conditions':['is_ready_to_move_on', 'guess_equals_number']},
+                { 'trigger': 'advance', 'source': 'make_guess', 'dest': 'higher_or_lower', 'unless':['is_ready_to_move_on', 'guess_equals_number'], 'before':'give_feedback' },
+                { 'trigger': 'advance', 'source': 'make_guess', 'dest': 'make_guess', 'conditions':'is_ready_to_move_on', 'unless':'guess_equals_number', 'before':'incorrect_guess_response' }, #participant says unmber is correct when it is not
+                { 'trigger': 'advance', 'source': 'make_guess', 'dest': 'make_guess', 'conditions':'guess_equals_number', 'unless':'is_ready_to_move_on', 'before':'incorrect_guess_response' }, #participant says number is not correct when it is
+
+                { 'trigger': 'advance', 'source': 'higher_or_lower', 'dest': 'make_guess', 'conditions':'is_ready_to_move_on',  'before':'give_feedback_calculate_guess' },
+                { 'trigger': 'advance', 'source': 'higher_or_lower', 'dest': 'higher_or_lower', 'unless':'is_ready_to_move_on', 'before':'incorrect_higher_lower'},
+
+                { 'trigger': 'advance', 'source': 'play_again', 'dest': 'end', 'conditions':'is_ready_to_move_on'},
+                { 'trigger': 'advance', 'source': 'play_again', 'dest': 'input_number', 'unless':'is_ready_to_move_on'},
+
+                { 'trigger':'abort', 'source':'*', 'dest':'end'} #all states go to the end if the button is pressed
+            ]
+
+game = NumberGameInteraction(True)
+machine = Machine(model=game, states=states, transitions=transitions, initial='get_name', before_state_change='reset_ready_to_move_on', show_state_attributes=True, show_conditions=True)
+
+game.get_graph().draw('TommyThumbDiagram.png', prog='dot')
+
+while(1):
+    print(game.state)
+    print('to')
+    game.advance()
+    print(game.state)
+    print('---------')
--- Final Versions/feedback.py
+++ Final Versions/feedback.py
@@ -2,15 +2,42 @@ import time
 import random
 import math
 
-class feedbackController:
+class statementRandomizer:
     def __init__(self):
-        self.supinateList = []
-        self.pronateList = []
-        self.performedBehaviors = [[],[],[],[],[]]
+        self.supinateList = [999] #will hold the 10 most recent values for supination
+        self.pronateList = [-999] #will hold the 10 most recent values for pronation
+        self.performedBehaviors = [[],[],[],[],[],[]]
+
+        self.guessStatements = {
+            1: "guess1",
+            2: "guess2",
+            3: "guess3",
+            4: "guess4",
+            5: "guess5",
+            6: "guess6",
+            7: "guess7",
+            8: "guess8",
+            9: "guess9",
+            10: "guess10",
+            11: "guess11",
+            12: "guess12",
+            13: "guess13"
+        }
 
         self.bucket1 = {
             1: "clarify1",
-            2: "clarify2"
+            2: "clarify2",
+            3: "clarify3",
+            4: "clarify4",
+            5: "clarify5",
+            6: "clarify6",
+            7: "clarify7",
+            8: "clarify8",
+            9: "clarify9",
+            10: "clarify10",
+            11: "clarify11",
+            12: "clarify12",
+            13: "clarify13"
         }
 
 
@@ -18,62 +45,112 @@ class feedbackController:
             1: "encourage1",
             2: "encourage2",
             3: "encourage3",
-            4: "encourage4"
+            4: "encourage4",
+            5: "encourage5",
+            6: "encourage6",
+            7: "encourage7",
+            8: "encourage8",
+            9: "encourage9",
+            10: "encourage10",
+            11: "encourage11",
+            12: "encourage12",
+            13: "encourage13"
         }
 
         self.bucket3 = {
-            1: "encourageLess1",
-            2: "encourageLess2",
-            3: "encourageLess3"
+            1: "encourageless1",
+            2: "encourageless2",
+            3: "encourageless3",
+            4: "encourageless4",
+            5: "encourageless5",
+            6: "encourageless6",
+            7: "encourageless7",
+            8: "encourageless8",
+            9: "encourageless9",
+            10: "encourageless10",
+            11: "encourageless11",
+            12: "encourageless12",
+            13: "encourageless13"
         }
 
         self.bucket4 = {
-            1: "rewardLess1",
-            2: "rewardLess2",
-            3: "rewardLess3",
-            4: "rewardLess4",
-            5: "rewardLess5"
+            1: "rewardless1",
+            2: "rewardless2",
+            3: "rewardless3",
+            4: "rewardless4",
+            5: "rewardless5",
+            6: "rewardless6",
+            7: "rewardless7",
+            8: "rewardless8",
+            9: "rewardless9",
+            10: "rewardless10",
+            11: "rewardless11",
+            12: "rewardless12",
+            13: "rewardless13"
         }
 
         self.bucket5 = {
             1: "reward1",
-            2: "reward2"
+            2: "reward2",
+            3: "reward3",
+            4: "reward4",
+            5: "reward5",
+            6: "reward6",
+            7: "reward7",
+            8: "reward8",
+            9: "reward9",
+            10: "reward10",
+            11: "reward11",
+            12: "reward12",
+            13: "reward13"
         }
 
-    def chooseRandom(self, bucketNumber):
-        upperBound = 0
-        behaviorDict = {}
+    def chooseRandomStatement(self, statementType):
+        mapping = {
+            0: self.guessStatements,
+            1: self.bucket1,
+            2: self.bucket2,
+            3: self.bucket3,
+            4: self.bucket4,
+            5: self.bucket5
+        }
+        #convert statement type to actual dictionary
+        behaviorDict = mapping[statementType]
+        upperBound = len(behaviorDict.keys())
 
-        if(bucketNumber == 1):
-            upperBound = len(self.bucket1.keys())
-            behaviorDict = self.bucket1
-        if(bucketNumber == 2):
-            upperBound = len(self.bucket2.keys())
-            behaviorDict = self.bucket2
-        if(bucketNumber == 3):
-            upperBound = len(self.bucket3.keys())
-            behaviorDict = self.bucket3
-        if(bucketNumber == 4):
-            upperBound = len(self.bucket4.keys())
-            behaviorDict = self.bucket4
-        if(bucketNumber == 5):
-            upperBound = len(self.bucket5.keys())
-            behaviorDict = self.bucket5
+        #clear half of the oldest values from the list of played animations if they have all been played
+        if(upperBound == len(self.performedBehaviors[statementType])):
+            while len(self.performedBehaviors[statementType]) > upperBound/2:
+                self.performedBehaviors[statementType].pop(0)
 
-        if(upperBound == len(self.performedBehaviors[bucketNumber - 1])):
-            self.performedBehaviors[bucketNumber - 1] = []
-
+        #determine the number of the behavior to play, and
+        #choose a new behavior if the desired number has already been played
         behaviorNumber = random.randint(1,upperBound)
-        while(behaviorNumber in self.performedBehaviors[bucketNumber-1]):
+        while(behaviorNumber in self.performedBehaviors[statementType]):
             behaviorNumber = random.randint(1,upperBound)
 
-        self.performedBehaviors[bucketNumber-1].append(behaviorNumber)
+        #once the number is found, return the key value for the CoRDial statement
+        self.performedBehaviors[statementType].append(behaviorNumber)
         return behaviorDict[behaviorNumber]
 
 
-    def getBehavior(self, thumb_angle, gesture_time):
-        supinate = True
-        pronate = False
+    def getResponseBehavior(self, thumb_angle, gesture_time):
+        supinate = False
+        pronate = True
+        rewardOverride = False
+
+        #current angle is the best seen so far
+        if(pronate and max(self.pronateList) < thumb_angle) or (supinate and min(self.supinateList) > thumb_angle):
+            print('that was the current best in the past  10 runs!')
+            rewardOverride = True
+
+        if(pronate and len(self.pronateList) > 3 and self.pronateList[-1] < thumb_angle and self.pronateList[-2] < self.pronateList[-1] and self.pronateList[-3] < self.pronateList[-2]):
+            print('that showed improvement over the past three trials')
+            rewardOverride = True
+
+        if(supinate and len(self.supinateList) > 3 and self.supinateList[-1] > thumb_angle and self.supinateList[-2] > self.supinateList[-1] and self.supinateList[-3] > self.supinateList[-2]):
+            print('that showed improvement over the past three trials')
+            rewardOverride = True
 
         if(supinate):
             appendValue(self.supinateList, thumb_angle)
@@ -82,18 +159,17 @@ class feedbackController:
         else:
             print('Uh-Oh, that is neither supinate nor pronate')
 
-        #current angle is the best seen so far
-        if(pronate and max(self.pronateList) == thumb_angle) or (supinate and min(self.supinateList) == thumb_angle):
-            behavior = self.chooseRandom(5) #maybe we want a seperate set of improved behaviors?
+        if rewardOverride:
+            return self.chooseRandomStatement(5) #maybe we want a seperate set of improved behaviors?
 
         #make the buckets based on the GAS variables
         angle_bucket = determine_angle_GAS_bucket(thumb_angle)
         time_bucket = determine_time_GAS_bucket(gesture_time)
         print('angle bucket is {}, time bucket is {}'.format(angle_bucket, time_bucket))
-        case = int(math.ceil((angle_bucket + time_bucket) / 2))
+        bucket = int(math.ceil((angle_bucket + time_bucket) / 2))
 
         #return the behavior key
-        return self.chooseRandom(case)
+        return self.chooseRandomStatement(bucket)
 
 
 def appendValue(listToAppend, valueToAppend):
@@ -109,7 +185,7 @@ def determine_angle_GAS_bucket(thumb_angle):
 
     for index in range(len(GAS_angle_scores)):
         if thumb_angle < GAS_angle_scores[index]:
-            return index + 1
+            return index
     #if we get here, the thumb angle is higher than the highest GAS score
     return len(GAS_angle_scores)
 
@@ -119,22 +195,17 @@ def determine_time_GAS_bucket(gesture_time):
 
     for index in range(len(GAS_time_scores)):
         if gesture_time > GAS_time_scores[index]:
-            return index + 1
+            return index
     #if we get here, the thumb angle is higher than the highest GAS score
     return len(GAS_time_scores)
 
-
-
-
-# fb = feedbackController()
-# start_time = time.time()
 #
-# while 1:
-#     thumb_angle = random.randint(1,60)
-#     gesture_time = random.randint(1,50)*0.1
+# sr = statementRandomizer()
 #
-#     print('angle: {}, time: {}'.format(thumb_angle, gesture_time))
+# while(1):
+#     listID = input('which list?')
+#     sr.getResponseBehavior(60 - listID*10, listID)
+#     for i in range(0, len(sr.performedBehaviors)):
+#         print(sr.performedBehaviors[i])
 #
-#     fb.getBehavior(thumb_angle, gesture_time, time.time() - start_time, 'nathan')
-#     for i in range(1,6):
-#         print('Bucket{}: {}'.format(i, fb.performedBehaviors[i-1]))
+#     print('-------------------------------')
